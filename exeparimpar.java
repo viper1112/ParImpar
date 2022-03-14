20fc86275446906168e26d9ba8ac48177b8d02bc
Viper <57271177+viper1112@users.noreply.github.com>
2022-03-14 11:36:53 -0300

    Add files via upload

---
+++ exeparimpar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exeparimpar {
+    
+            public static void main(String[] args) {
+                Scanner numero = new Scanner(System.in);
+                
+                System.out.print("Digite um numero ");
+                int n1 = numero.nextInt();
+                
+                if(n1 % 2 == 0){
+                    System.out.println("Seu numero é par");
+                }else{
+                    System.out.println("Seu numero é ímpar");
+                }
+                numero.close();
+                
+    }
+    
+}
